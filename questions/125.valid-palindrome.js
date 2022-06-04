48776fb695a4273dc7642e3b1ffbaf04a6e34c52
Peter Kwan <peterkwkwan@gmail.com>
2022-06-04 20:55:17 +0800

    optimized palindrome solution

--- questions/125.valid-palindrome.js
+++ questions/125.valid-palindrome.js
@@ -12,15 +12,29 @@
 var isPalindrome = function(s) {
     const validString = s.replace(/[^0-9a-z]/gi, '').toLowerCase()
 
-    for(let i = 0; i <= validString.length / 2 - 1; i ++){
-        for(let j = validString.length - 1; j > i; j --){
-            if(validString[j] !== validString[i]){
-                return false
-            }
-            i++;
+    // for(let i = 0; i <= validString.length / 2 - 1; i ++){
+    //     for(let j = validString.length - 1; j > i; j --){
+    //         if(validString[j] !== validString[i]){
+    //             return false
+    //         }
+    //         i++;
+    //     }
+    // }
+    // return true
+
+    let left = 0
+    let right = validString.length - 1
+
+    while (left <= right){
+        if(validString[left] !== validString[right]){
+            return false
         }
+        left ++
+        right --
     }
+
     return true
+
 };
 // @lc code=end
 
