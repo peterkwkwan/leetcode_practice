fd51a36e8848b57aafdae9cc8046a37438bf2577
Peter Kwan <peterkwkwan@gmail.com>
2022-06-08 12:01:16 +0800

    set method for longest substring

--- questions/3.longest-substring-without-repeating-characters.js
+++ questions/3.longest-substring-without-repeating-characters.js
@@ -10,27 +10,45 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let currentString = ''
+    // let currentString = ''
+
+    // let longestCount = 0
+
+    // for(let i = 0; i < s.length; i++){
+    //     if(!currentString.includes(s[i])){
+    //         currentString += s[i]
+    //         longestCount = Math.max(currentString.length, longestCount)
+    //     } else {
+    //         currentString += s[i]
+    //         for(let j = 0; j < currentString.length; j++){
+    //             if(currentString[j] === s[i]){
+    //                 currentString = currentString.substring(j + 1, currentString.length)
+    //                 break
+    //             }
+    //         }
+    //     }
+    // }
+
+    // return longestCount
+
+
+    const stringSet = new Set()
+
+    let start = 0
 
     let longestCount = 0
 
     for(let i = 0; i < s.length; i++){
-        if(!currentString.includes(s[i])){
-            currentString += s[i]
-            longestCount = Math.max(currentString.length, longestCount)
-        } else {
-            currentString += s[i]
-            for(let j = 0; j < currentString.length; j++){
-                if(currentString[j] === s[i]){
-                    currentString = currentString.substring(j + 1, currentString.length)
-                    break
-                }
-            }
+        while(stringSet.has(s[i])){
+            stringSet.delete(s[start])
+            start++
         }
+        stringSet.add(s[i])
+        longestCount = Math.max(longestCount, (i - start + 1))
     }
 
-    return longestCount
 
+    return longestCount
 };
 // @lc code=end
 
